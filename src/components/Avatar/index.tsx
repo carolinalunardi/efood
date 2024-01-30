8139e416766f5e6450d67813fb0f1908e80deb7c
CarolinaLunardi <lunardicll@gmail.com>
2024-01-30 15:57:59 +0000

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/carolinalunardi.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/carolinalunardi.png" />
-    <Titulo fontSize={20}>Carolina Lunardi</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Carolina Lunardi</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        carolinalunardi
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Full Stack Designer
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
